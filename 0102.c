cbf4e195ac288fb1fd1e2def83bcdb66170e3d32
Yisu Wang <ywang30@cs.tufts.edu>
2014-12-23 17:52:20 -0500

    first commit for cpl

---
+++ 0102.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+        printf("hello, world\n");
+        printf("hello, world\e");
+        printf("hello, world\r");
+        printf("hello, world\t");
+        printf("hello, world\a");
+        printf("hello, world\f");
+        printf("hello, world\f");
+}
